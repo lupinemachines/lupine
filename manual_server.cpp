479df12ba4485f22d0e0d70939fbd9bb1cc8efc4
Kevin Wang <kevmo314@gmail.com>
2026-06-07 00:43:38 -0400

    Avoid Windows min max macros

--- manual_server.cpp
+++ manual_server.cpp
@@ -13,6 +13,12 @@
 #include <unordered_set>
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
 #endif
 
