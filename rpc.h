4e9a166c96f8cef76d7b7dd1bc5381ae4527c5d6
Kevin Wang <kevmo314@gmail.com>
2026-06-11 02:09:54 -0400

    Add protocol version handshake at connect time

--- rpc.h
+++ rpc.h
@@ -30,6 +30,9 @@ extern int rpc_read_end(conn_t *conn);
 
 extern int rpc_wait_for_response(conn_t *conn);
 
+extern int rpc_handshake(conn_t *conn);
+extern int rpc_handshake_failed(void);
+
 extern int rpc_write_start_request(conn_t *conn, const int op);
 extern int rpc_write_start_response(conn_t *conn, const int read_id);
 extern int rpc_write(conn_t *conn, const void *data, const size_t size);
