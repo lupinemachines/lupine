63921ddd5e9f038a8f4efd80b66b67e620dda718
Kevin Wang <kevmo314@gmail.com>
2026-08-25 00:00:43 -0400

    Classify unified pointers and pack pitched host readbacks in 2D/3D copies (#546)

--- memcpy.h
+++ memcpy.h
@@ -4,6 +4,13 @@
 #include "cuda_compat.h"
 #undef LUPINE_CUDA_COMPAT_TYPES_ONLY
 
+enum class lupine_copy_direction {
+  host_to_host,
+  host_to_device,
+  device_to_host,
+  device_to_device,
+};
+extern "C" bool lupine_copy_pointer_is_host(CUdeviceptr ptr);
 extern "C" bool lupine_is_managed_host_alias(CUdeviceptr ptr);
 extern "C" CUresult lupine_sync_mapped_device_to_host();
 extern "C" void lupine_materialize_host_allocations();
